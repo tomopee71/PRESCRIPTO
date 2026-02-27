dfcb65982a0630c74fb9df459d31def6667e635f
tomopee71 <possiblevigour@gmail.com>
2026-02-28 07:13:46 +0900

    1回目のプッシュ

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-green-500 mt-3'>Welcome to the App!</h1>
+
+    </div>
+  )
+}
+
+export default App
